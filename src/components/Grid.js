983fb32533bbcbd824533a8e024acc1e8466ce66
Nick Berliner <nick.berliner96@gmail.com>
2021-01-19 15:15:05 +0100

    Date filter working!

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -10,11 +10,37 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
 
-const endDate = new Date();
-const startDate = new Date();
+const endTime = new Date();
+const startTime = new Date();
 // Default start date is 10 weeks ago 
-startDate.setDate(startDate.getMonth() - 70);
+startTime.setDate(startTime.getMonth() - 70);
 
+const filterParams = {
+    comparator: function (filterLocalDateAtMidnight, cellValue) {
+        let dateAsString = cellValue;
+        if ( dateAsString == null ) {
+            return -1;
+        }
+        let getRidOfTime = dateAsString.split('T')[0];
+        let dateParts = getRidOfTime.split('-');
+        let cellDate = new Date(
+            Number(dateParts[0]),
+            Number(dateParts[1]) - 1,
+            Number(dateParts[2])
+        );
+        if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) {
+            return 0;
+        }
+        if (cellDate < filterLocalDateAtMidnight) {
+            return -1;
+        }
+        if (cellDate > filterLocalDateAtMidnight) {
+            return 1;
+        }
+    },
+    browserDatePicker: true,
+    minValidYear: 2000,
+  };
 export default class Grid extends React.Component {
     constructor(props) {
         super(props);
@@ -23,46 +49,112 @@ export default class Grid extends React.Component {
                 { headerName: 'ID', field: 'id' },
                 { headerName: 'Type of Goods', field: 'type_of_goods' },
                 { headerName: 'CO2 Emitted', field: 'total_co2_emitted' },
-                { headerName: 'Weight', field: 'weight' }
+                { headerName: 'Weight', field: 'weight' },
+                { 
+                    headerName: 'Date', 
+                    field: 'start_time',
+                    filter: 'agDateColumnFilter',
+                    filterParams: filterParams
+                }
             ],
             rowData: [],
-            startDate: startDate,
-            endDate: endDate
+            // rowData: null,
+            dateFilteredData: [],
+            startTime: startTime,
+            endTime: endTime,
+            apiLoaded: false,
+            filtered: false
         };
-        this.handleStartDateChange = this.handleStartDateChange.bind(this);
-        this.handleEndDateChange = this.handleEndDateChange.bind(this);
+        this.handleStartTimeChange = this.handleStartTimeChange.bind(this);
+        this.handleEndTimeChange = this.handleEndTimeChange.bind(this);
+        this.filterData = this.filterData.bind(this);
     }
+
+    // onGridReady = (params) => {
+    //     this.gridApi = params.api;
+    //     this.gridColumnApi = params.columnApi;
+    
+    //     const httpRequest = new XMLHttpRequest();
+    //     const updateData = (data) => {
+    //       this.setState({ rowData: data });
+    //     };
+    
+    //     httpRequest.open(
+    //       'GET',
+    //       `https://raw.githubusercontent.com/nickberliner1/tracks-data/master/shipments-2.json`
+    //     );
+    //     httpRequest.send();
+    //     httpRequest.onreadystatechange = () => {
+    //       if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+    //         updateData(JSON.parse(httpRequest.responseText));
+    //       }
+    //     };
+    //   };
 
     componentDidMount() {
         // add js to start from and end date/ time, then make date picture set state of start and end
         fetch(`https://raw.githubusercontent.com/nickberliner1/tracks-data/master/shipments-2.json`)
             .then(result => result.json())
-            .then(rowData => this.setState({rowData}))
+            .then(rowData => this.setState({rowData}));
+
+        this.apiLoaded = !this.apiLoaded;
     }
 
-    defaultEndDate() {
-        const date = new Date();
-        date.setDate(date.getMonth() - 70);
-        return date;
-    }
-
-    handleStartDateChange(date) {
+    handleStartTimeChange(date) {
         this.setState({
-            startDate: date
+            startTime: date
         })
+        this.gridApi.onFilterChanged();
+
     }
 
-    handleEndDateChange(date) {
+    handleEndTimeChange(date) {
         this.setState({
-            endDate: date
+            endTime: date
         })
+        this.gridApi.onFilterChanged();
+    }
+
+    filterData() {
+        const chosenDates = this.rowData.filter(function(item) {
+            let date = new Date(item.start_date);
+            return (date >= this.startTime && date <= this.endTime);
+        })
+        this.setState({
+            dateFilteredData: chosenDates
+        })
+        this.filtered = !this.filtered;
+    }
+
+    isExternalFilterPresent = () => {
+        if ( this.startTime.getTime() !== startTime.getTime() || 
+            this.endTime.getTime() !== endTime.getTime() ) {
+            return true;
+        }
+    }
+
+    doesExternalFilterPass = (node) => {
+        if ( node.start_time.getTime() > this.startTime.getTime() || 
+            node.end_time.getTime() < this.endTime.getTime() ) {
+                return true;
+        } else {
+            return false;
+        }
+        // switch (startTime) {
+        //     case this.startTime:
+        //         return node.start_time >= this.startTime;
+        // }
+        // switch (endTime) {
+        //     case this.endTime:
+        //         return node.end_time <= this.endTime;
+        // }
     }
 
     render() {
 
         const lightTheme = this.props.lightTheme;
-        const startDate = this.state.startDate;
-        const endDate = this.state.endDate;
+        const startTime = this.state.startTime;
+        const endTime = this.state.endTime;
         const rowData = this.state.rowData;
         const columnDefs = this.state.columnDefs;
 
@@ -71,29 +163,46 @@ export default class Grid extends React.Component {
                 <div className={`ag-theme-alpine${lightTheme ? '' : '-dark'}`}
                     style={{ height: '50vh', width: '60vw' }}
                 >
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <MuiPickersUtilsProvider 
+                        utils={DateFnsUtils}
+                    >
                         <KeyboardDatePicker
                             label='Start Date'
-                            value={startDate}
-                            onChange={this.handleStartDateChange}
+                            value={startTime}
+                            onChange={this.handleStartTimeChange}
                             animateYearScrolling
                         />
                         <KeyboardDatePicker
                             label='End Date'
-                            value={endDate}
-                            onChange={this.handleEndDateChange}
+                            value={endTime}
+                            onChange={this.handleEndTimeChange}
                             animateYearScrolling
                         />
                     </MuiPickersUtilsProvider>
+<button onClick={this.filterData}>filter</button>
+                    {/* <div>
+                    {rowData
+                        // .filter(item =>
+                        //     item.start_time >= startTime &&
+                        //     item.end_time <= endTime)
+                        .map(item => (
+                            <div>{item.id}</div>
+                        ))        
+                    }
+                    </div> */}
                     <AgGridReact
                         lightTheme={lightTheme}
-                        rowData={rowData}
+                        // rowData={this.state.dateFilteredData}
+                        rowData={this.state.filtered ? this.state.filterData : rowData}
                         columnDefs={columnDefs}
                         enableSorting={true}
                         filter={true}
                         pagination={true}
                         paginationPageSize={10}
                         colResizeDefault='shift'
+                        onGridReady={this.onGridReady}
+                        // isExternalFilterPresent={this.isExternalFilterPresent}
+                        // doesExternalFilterPass={this.doesExternalFilterPass}
                     ></AgGridReact>
 
                 </div>
